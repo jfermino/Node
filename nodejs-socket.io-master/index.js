b5ba3d0707f85848a160c01aa18a5dac1ffc218b
Júlio Cesar Dos Santos Fermino <1228875@sga.pucminas.br>
2019-05-21 21:49:00 -0300

    commit exercicio 5

--- nodejs-socket.io-master/index.js
+++ nodejs-socket.io-master/index.js
@@ -2,13 +2,39 @@ const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http);
 
+var mensage = {
+    pergunta: "",
+    pos: -1,
+    listRespostas: []
+};
+
+var listPerguntas = ["Qual seu nome ?","Qual seu endereço ?","Qual sua idade ?","Qual seu e-mail ?","Qual sua profissão ?"]
+
 app.get('/', (_, res) => {
-  res.sendFile(`${__dirname}/index.html`)
+    res.sendFile(`${__dirname}/index.html`)
 })
 
 io.on('connection', socket => {
+    if(mensage.pos == -1 || mensage.listRespostas.length == 0)
+    {
+        mensage.pos = 0;
+    }
+        
+
+    mensage.pergunta = listPerguntas[mensage.pos]
+
+    io.emit('messageResponse',mensage);
     socket.on('newMessage', msg => {
-        io.emit('messageResponse', msg)
+       
+    mensage.listRespostas.push(listPerguntas[mensage.pos] +" "+msg);
+    mensage.pos = mensage.pos + 1;
+    if(mensage.pos < listPerguntas.length ){
+        mensage.pergunta = listPerguntas[mensage.pos]
+    }else{
+        mensage.pos = -100;
+    }
+
+        io.emit('messageResponse', mensage);
     })
 })
 
